2f55f314a829a5f409c4bbf21440c7c76e0a198c
syt88 <syt8886@gmail.com>
2021-08-02 18:44:17 +0900

    project

--- app/src/androidTest/java/com/example/where/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/where/ExampleInstrumentedTest.java
@@ -2,10 +2,8 @@ package com.example.where;
 
 import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
-//import androidx.test.ext.junit.runners.AndroidJUnit4;
-
 import org.junit.Test;
-import org.junit.runner.RunWith;
+
 
 import static org.junit.Assert.*;
 
@@ -14,12 +12,11 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-//@RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.taxiapp", appContext.getPackageName());
+        assertEquals("com.example.where", appContext.getPackageName());
     }
 }
